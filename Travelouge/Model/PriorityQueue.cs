13f6c42770ec7b8804ee93b1206021c53e9ecb4b
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-06-08 05:24:00 +0530

    adding backend stuff

---
+++ Travelouge/Model/PriorityQueue.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelouge.Model
+{
+
+    public class PriorityQueue<T>
+    {
+        private List<T> heap;
+        private IComparer<T> comparer;
+
+        public PriorityQueue()
+        {
+            heap = new List<T>();
+            comparer = Comparer<T>.Default;
+        }
+
+        public PriorityQueue(IComparer<T> comparer)
+        {
+            heap = new List<T>();
+            this.comparer = comparer;
+        }
+
+        public void Enqueue(T item)
+        {
+            heap.Add(item);
+            SiftUp(heap.Count - 1);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Priority queue is empty");
+            }
+
+            T item = heap[0];
+            int lastIdx = heap.Count - 1;
+            heap[0] = heap[lastIdx];
+            heap.RemoveAt(lastIdx);
+            SiftDown(0);
+
+            return item;
+        }
+
+        public bool IsEmpty()
+        {
+            return heap.Count == 0;
+        }
+
+        private void SiftUp(int index)
+        {
+            while (index > 0)
+            {
+                int parentIdx = (index - 1) / 2;
+                if (comparer.Compare(heap[index], heap[parentIdx]) > 0)
+                {
+                    Swap(index, parentIdx);
+                    index = parentIdx;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void SiftDown(int index)
+        {
+            int size = heap.Count;
+            while (index < size)
+            {
+                int leftChildIdx = 2 * index + 1;
+                int rightChildIdx = 2 * index + 2;
+
+                int largest = index;
+                if (leftChildIdx < size && comparer.Compare(heap[leftChildIdx], heap[largest]) > 0)
+                {
+                    largest = leftChildIdx;
+                }
+                if (rightChildIdx < size && comparer.Compare(heap[rightChildIdx], heap[largest]) > 0)
+                {
+                    largest = rightChildIdx;
+                }
+
+                if (largest != index)
+                {
+                    Swap(index, largest);
+                    index = largest;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Swap(int i, int j)
+        {
+            T temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
+        }
+    }
+
+}
+
--- Travelouge/UserControls/PackingList.xaml.cs
+++ Travelouge/UserControls/PackingList.xaml.cs
@@ -12,20 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Travelouge.Model;
 
-
-namespace Travelouge
+namespace Travelouge.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PackingList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PackingList : UserControl
     {
-        public MainWindow()
+        public PackingList()
         {
             InitializeComponent();
-            
         }
     }
 }
